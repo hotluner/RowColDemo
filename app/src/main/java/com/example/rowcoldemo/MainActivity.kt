3cc068d49a186e73433b233e0106df07da15404a
hotluner <dimkoval1@gmail.com>
2025-10-07 14:50:26 +0700

    3. row 1st

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.*
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -48,7 +49,11 @@ fun TextCell(text: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-
+    Row(modifier) {
+        TextCell("1")
+        TextCell("2")
+        TextCell("3")
+    }
 }
 
 
